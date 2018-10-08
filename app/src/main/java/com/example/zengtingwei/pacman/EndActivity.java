5911bea9fd1224ab88e65aaf1afe05d754d7be64
zengtingwei <u6225609@anu.edu.au>
2018-10-08 23:35:51 +1100

    add a pic and two buttons in end activity

--- app/src/main/java/com/example/zengtingwei/pacman/EndActivity.java
+++ app/src/main/java/com/example/zengtingwei/pacman/EndActivity.java
@@ -1,7 +1,10 @@
 package com.example.zengtingwei.pacman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class EndActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class EndActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end);
+    }
+
+    public void restart(View view){
+        Log.d("Restart", "Game restart");
+        Intent it = new Intent(this, GameActivity.class);
+        startActivity(it);
+    }
+
+    public void menu(View view){
+        Log.d("Menu", "Return to menu");
+        Intent it = new Intent(this, WelcomeActivity.class);
+        startActivity(it);
     }
 }
---
+++ app/src/main/java/com/example/zengtingwei/pacman/EndView.java
@@ -0,0 +1,12 @@
+package com.example.zengtingwei.pacman;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class EndView extends View {
+    public EndView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
