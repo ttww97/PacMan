dea52f40a60cb5b99375a94e3b0b43f61a2b5598
Dylan <u6339535@anu.edu.au>
2018-10-18 21:00:42 +1100

    Minimax

--- app/src/main/java/com/example/zengtingwei/pacman/GameState.java
+++ app/src/main/java/com/example/zengtingwei/pacman/GameState.java
@@ -8,7 +8,15 @@ public class GameState {
 
     }
     static public double evaluation(int agent_x,int agent_y,int pac_x,int pac_y){
-        return Math.sqrt(Math.pow((agent_y-pac_y),2)+Math.pow((agent_x-pac_x),2));
+        double dis = Math.sqrt(Math.pow((agent_y-pac_y),2)+Math.pow((agent_x-pac_x),2));
+        int x_min = agent_x<pac_x?agent_x:pac_x;
+        int x_max = agent_x>pac_x?agent_x:pac_x;
+        int y_min = agent_y<pac_y?agent_y:pac_y;
+        int y_max = agent_y>pac_y?agent_y:pac_y;
+//        for(int i = x_min;i<=x_max;i++){
+//            if(Layout.wall[][])
+//        }
+        return dis;
     }
     static public int getBeanNum(Layout map){
         int bean_num=0;
@@ -76,23 +84,23 @@ public class GameState {
     static public Directions generateDirection(int agent_x,int agent_y,int pac_x,int pac_y,Layout map){
         double min = 5000000;
         Directions direction= Directions.RIGHT;
-        double dis_up = evaluation(agent_x,agent_y-50,pac_x,pac_y);
-        double dis_down=evaluation(agent_x,agent_y+50,pac_x,pac_y);
-        double dis_left=evaluation(agent_x-50,agent_y,pac_x,pac_y);
-        double dis_right=evaluation(agent_x+50,agent_y,pac_x,pac_y);
+        double dis_up = evaluation(agent_x,agent_y-1,pac_x,pac_y);
+        double dis_down=evaluation(agent_x,agent_y+1,pac_x,pac_y);
+        double dis_left=evaluation(agent_x-1,agent_y,pac_x,pac_y);
+        double dis_right=evaluation(agent_x+1,agent_y,pac_x,pac_y);
         if(dis_up < min && isMoveLegal(agent_x,agent_y,map,Directions.UP)){
             min = dis_up;
             direction = Directions.UP;
         }
-        if(dis_down < min-10  && isMoveLegal(agent_x,agent_y,map,Directions.DOWN)){
+        if(dis_down < min  && isMoveLegal(agent_x,agent_y,map,Directions.DOWN)){
             min = dis_down;
             direction = Directions.DOWN;
         }
-        if(dis_left < min-10  && isMoveLegal(agent_x,agent_y,map,Directions.LEFT)){
+        if(dis_left < min  && isMoveLegal(agent_x,agent_y,map,Directions.LEFT)){
             min = dis_left;
             direction = Directions.LEFT;
         }
-        if(dis_right < min-10 && isMoveLegal(agent_x,agent_y,map,Directions.RIGHT)){
+        if(dis_right < min && isMoveLegal(agent_x,agent_y,map,Directions.RIGHT)){
             min = dis_right;
             direction = Directions.RIGHT;
         }
